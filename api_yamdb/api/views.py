bc70480528b51b0ce28f5acb39d9bd7a70a14703
Unforgettable01 <corefano-manchelotti>
2023-12-17 19:25:47 +0300

    Каркас для сериализаторов и вьюшек

---
+++ api_yamdb/api/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import get_object_or_404
+
+from rest_framework import filters, mixins, permissions, viewsets
+
+from reviews.models import Category, Genre, Title
+from .serializers import CategorySerializer, GenreSerializer, TitleSerializer
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    queryset = Title.objects.all()
+    serializer_class = TitleSerializer
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
