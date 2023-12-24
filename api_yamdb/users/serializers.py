a5b1d521398241524da23db2d9621fd61f5b8035
Дмитрий Марков <markov0priz@gmail.com>
2023-12-24 22:36:38 +0300

    New fix users

--- api_yamdb/users/serializers.py
+++ api_yamdb/users/serializers.py
@@ -1,11 +1,37 @@
+from django.conf import settings
+from django.core.validators import RegexValidator
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
+from rest_framework.validators import UniqueValidator
 
-from users.mixins import SerializerMixin
 from users.models import User
 
 
-class UserSerializer(SerializerMixin, serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        required=True,
+        max_length=settings.LIMIT_MAX,
+        validators=[
+            UniqueValidator(
+                queryset=User.objects.all(),
+                message='Данный email уже используется.'
+            )
+        ]
+    )
+    username = serializers.CharField(
+        required=True,
+        max_length=settings.LIMIT_MED,
+        validators=[
+            UniqueValidator(
+                queryset=User.objects.all(),
+                message=('Данное username уже используется, '
+                         'придумайте другое username.')
+            ),
+            RegexValidator(
+                r'^[\w.@+-]+$'
+            )
+        ]
+    )
 
     class Meta:
         model = User
@@ -13,17 +39,41 @@ class UserSerializer(SerializerMixin, serializers.ModelSerializer):
                   'email', 'bio', 'role')
 
 
-class UserRegistrationSerializer(SerializerMixin, serializers.ModelSerializer):
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        required=True,
+        max_length=settings.LIMIT_MAX,
+    )
+    username = serializers.CharField(
+        required=True,
+        max_length=settings.LIMIT_MED,
+        validators=[
+            RegexValidator(
+                r'^[\w.@+-]+$'
+            )
+        ]
+    )
 
     class Meta:
         model = User
         fields = ('email', 'username')
 
-    def validate_username(self, username):
+    def validate(self, data):
+        username = data['username']
+        email = data['email']
         if username.lower() == 'me':
             raise ValidationError(f'Запрещено использовать имя {username}'
                                   f'в качестве username!')
-        return username
+        elif (User.objects.filter(username=username).exists()
+                and not User.objects.filter(email=email).exists()):
+            raise serializers.ValidationError(
+                'Данное username уже используется, '
+                'придумайте другое username.')
+        elif (User.objects.filter(email=email).exists()
+                and not User.objects.filter(username=username).exists()):
+            raise serializers.ValidationError(
+                'Данный email уже используется.')
+        return data
 
 
 class ConfirmationCodeSerializer(serializers.Serializer):
