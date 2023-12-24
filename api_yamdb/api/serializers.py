3ec141495106b319cc5a713d837e986de859eff0
Rodion <rodsmir2005@yandex.ru>
2023-12-24 23:08:03 +0300

    Fixed Grammar Mistakes

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -58,9 +58,9 @@ class ReviewSerializer(serializers.ModelSerializer):
 
     def validate(self, data):
         if self.context.get('request').method == 'POST':
-            user = self.context.get('request').use
+            user = self.context.get('request').user
             title_id = self.context.get('view').kwargs.get('title_id')
-            if Review.objects.filter(author=user, itle=title_id).exists():
+            if Review.objects.filter(author=user, title=title_id).exists():
                 raise forms.ValidationError(
                     'Этот пользователь уже оставлял отзыв!')
         return data
